b9268cac6b9e8a4931c44b7a98a05d97ad917efb
Ethanator <ychen18@nd.edu>
2014-12-10 03:55:10 +0000

    move open and close into the loop

--- make_videos.py
+++ make_videos.py
@@ -4,17 +4,17 @@ import os
 import datetime
 
 times = []
-f = open('time.txt', 'w')
 for i in xrange(5):
     start = datetime.datetime.now()
     for j in xrange(10):
         os.system('povray +Irubiks.pov +Oframe' + str(i) + '-' + '%03d' % (j) + '.png +W320 +H400 +K' + str(float(j) / 10)) 
     os.system('ffmpeg -r 10 -i frame' + str(i) + '-%03d.png -r ntsc movie-' + str(i) + '.mpg')
     t = (datetime.datetime.now() - start).total_seconds()
+    f = open('time.txt', 'w')
     f.write(str(t) + '\n')
+    f.close()
     times.append(t)
     os.system('rm frame' + str(i) + '*')
-f.close()
 print '==='
 for t in times:
     print t
