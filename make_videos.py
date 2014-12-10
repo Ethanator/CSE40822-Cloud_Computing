3ea57b5036d63f0b068a8880d6c59779a21087ef
Ubuntu <ubuntu@ip-172-31-5-61.us-west-2.compute.internal>
2014-12-10 02:54:17 +0000

    add a missing left bracket

--- make_videos.py
+++ make_videos.py
@@ -10,7 +10,7 @@ for i in xrange(5):
     for j in xrange(10):
         os.system('povray +Irubiks.pov +Oframe' + str(i) + '-' + '%03d' % (j) + '.png +W320 +H400 +K' + str(float(j) / 10)) 
     os.system('ffmpeg -r 10 -i frame' + str(i) + '-%03d.png -r ntsc movie-' + str(i) + '.mpg')
-    t = datetime.datetime.now() - start).total_seconds()
+    t = (datetime.datetime.now() - start).total_seconds()
     f.write(str(t) + '\n')
     times.append(t)
     os.system('rm frame' + str(i) + '*')
