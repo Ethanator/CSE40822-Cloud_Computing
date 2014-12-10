f5fe86de0d2a16e996098a8cec63d5724698082a
Ubuntu <ubuntu@ip-172-31-5-61.us-west-2.compute.internal>
2014-12-10 02:50:29 +0000

    fix the output-to-file part

--- make_videos.py
+++ make_videos.py
@@ -4,17 +4,18 @@ import os
 import datetime
 
 times = []
+f = open('time.txt', 'w')
 for i in xrange(5):
     start = datetime.datetime.now()
     for j in xrange(10):
         os.system('povray +Irubiks.pov +Oframe' + str(i) + '-' + '%03d' % (j) + '.png +W320 +H400 +K' + str(float(j) / 10)) 
     os.system('ffmpeg -r 10 -i frame' + str(i) + '-%03d.png -r ntsc movie-' + str(i) + '.mpg')
-    times.append((datetime.datetime.now() - start).total_seconds())
+    t = datetime.datetime.now() - start).total_seconds()
+    f.write(str(t) + '\n')
+    times.append(t)
     os.system('rm frame' + str(i) + '*')
+f.close()
 print '==='
-f = open('time.txt', 'w')
 for t in times:
     print t
-    f.write(str(t) + '\n')
-f.close()
     
